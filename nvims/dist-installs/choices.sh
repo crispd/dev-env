d4c6531bf397f99c207c929128eb2fb2d3138913
Dan Crisp <github@contact-alias.pro>
2023-10-16 09:37:53 -0500

    Some small tweaks and updates to comments in nvims stuff

--- nvims/dist-installs/choices.sh
+++ nvims/dist-installs/choices.sh
@@ -5,11 +5,17 @@
 # 	NvChad
 # 	kickstart
 # 	LunarVim
-# 	...there are many others...
+# 	*there are more...*
 
+# This file's path
 DIST_INSTALLS="$SCRIPT_PATH/nvims/dist-installs"
 
+# List of distros to install (each must have an install script in $DIST_INSTALLS)
 declare -a distros=("LazyVim")
+
+# Run the install script for each distro in the list 'distros' from above.
+# 	For each that is ran, ensure all output is captured in a logfile
+# 	of name <distro>.log
 for i in "${distros[@]}"
 do
 	#source "$DIST_INSTALLS/$i.sh" 2>&1 | tee -a "$DIST_INSTALLS/$i.log"
--- nvims/install.sh
+++ nvims/install.sh
@@ -3,10 +3,10 @@
 # Installing nvim distributions like LazyVim, AstroVim, NvChad, etc.
 source "$SCRIPT_PATH/nvims/dist-installs/choices.sh"
 
-# Anything in ~/.profile.d/ with the same name as what's in ./profile.d/ is backed up
-# 	and a symlink is added to ~/.profile.d/ for each file found in ./profile.d/
+# Anything existing in ~/.profile.d/ with the same name as what's in dev-env/nvims/profile.d/ is backed up
+# 	and a symlink is added to ~/.profile.d/ for each file found in dev-env/nvims/profile.d/
 for i in "$SCRIPT_PATH/nvims/profile.d/"*; do
-	if [[ ! -e ~/.profile.d/$(basename "$i") ]]; then
+	if [ -e ~/.profile.d/$(basename "$i") ]; then
 		mv ~/.profile.d/$(basename "$i") ~/.profile.d/$(basename "$i").bak
 	fi
 	ln -fns "$SCRIPT_PATH/nvims/profile.d/$(basename "$i")" "$HOME/.profile.d/$(basename "$i")"
